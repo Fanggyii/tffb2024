e95ef825f26628c290c8031d8485b8c407eff658
Munus Shih <munusshih@newschool.edu>
2023-09-19 23:57:31 -0400

    Make Logo Bigger

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,9 +1,9 @@
 export default async function Sponsors(props) {
     const { language, sponsors } = props
     return (
-        <div className="grid grid-cols-3 md:grid-cols-5 gap-8 pt-10 text-center">
+        <div className="grid grid-cols-3 gap-10 text-center">
             {sponsors.map(sponsor => <div className="sponsor">
-                <div className="flex block mx-auto w-full h-full md:w-1/2 md:h-1/2 mb-10">
+                <div className="flex mx-auto w-[80%] h-[80%] mb-10">
                     <img className="w-full m-auto object-fill" src={sponsor.fields['Img']}></img>
                 </div>
             </div>)}
