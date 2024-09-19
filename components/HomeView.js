d2d8be4ff5514b6851df7f56d520c478e3116df1
lowpolyfox <alan.maldonado.lara1@gmail.com>
2024-09-19 16:28:03 +0200

    Add nullish coalescense operator to fix undefined references breaking the build

--- components/HomeView.js
+++ components/HomeView.js
@@ -1,190 +1,256 @@
-import LanguageSelect from "./LanguageSelect";
-import Marquee from "./Marquee";
-import Modal from "./Modal";
-import Questions from "./Questions";
-import SectionTitle from "./SectionTitle";
-import SpecialTitle from "./SpecialTitle";
-import Film from "./Film";
-import Events from "./Events";
-import Sponsors from "./Sponsors";
-import Calendar from "./MyCalendar";
-import Footer from "./Footer";
-import dropboxUrl from "../utils/dropboxUrl";
-import { sectionTitles, isEmpty } from "../utils/helpers";
-import dynamic from 'next/dynamic'
-import RichText from "./RichText";
-import P5preloader from "./P5preloader";
+import LanguageSelect from './LanguageSelect';
+import Marquee from './Marquee';
+import Modal from './Modal';
+import Questions from './Questions';
+import SectionTitle from './SectionTitle';
+import SpecialTitle from './SpecialTitle';
+import Film from './Film';
+import Events from './Events';
+import Sponsors from './Sponsors';
+import Calendar from './MyCalendar';
+import Footer from './Footer';
+import dropboxUrl from '../utils/dropboxUrl';
+import { sectionTitles, isEmpty } from '../utils/helpers';
+import dynamic from 'next/dynamic';
+import RichText from './RichText';
+import P5preloader from './P5preloader';
 
-const airtableApiKey = process.env.AIRTABLE_API_KEY
-const airtableBaseId = process.env.AIRTABLE_BASE_ID
-const airtableTableId = process.env.AIRTABLE_TABLE_FILMS_ID
-const airtableTableFilmsViewId = process.env.AIRTABLE_TABLE_FILMS_VIEW_ID
-const airtableTableFilmEventId = process.env.AIRTABLE_TABLE_FILMEVENTS_ID
-const airtableTableFilmEventViewId = process.env.AIRTABLE_TABLE_FILMEVENTS_VIEW_ID
-const airtableTableOthersId = process.env.AIRTABLE_TABLE_OTHERS_ID
-const airtableTableOthersViewId = process.env.AIRTABLE_TABLE_OTHERS_VIEW_ID
-const airtableTableEventsId = process.env.AIRTABLE_TABLE_EVENTS_ID
-const airtableTableEventsViewId = process.env.AIRTABLE_TABLE_EVENTS_VIEW_ID
+const airtablePersonalAccessToken = process.env.AIRTABLE_ACCESS_TOKEN;
+const airtableBaseId = process.env.AIRTABLE_BASE_ID;
+const airtableTableId = process.env.AIRTABLE_TABLE_FILMS_ID;
+const airtableTableFilmsViewId = process.env.AIRTABLE_TABLE_FILMS_VIEW_ID;
+const airtableTableFilmEventId = process.env.AIRTABLE_TABLE_FILMEVENTS_ID;
+const airtableTableFilmEventViewId =
+	process.env.AIRTABLE_TABLE_FILMEVENTS_VIEW_ID;
+const airtableTableOthersId = process.env.AIRTABLE_TABLE_OTHERS_ID;
+const airtableTableOthersViewId = process.env.AIRTABLE_TABLE_OTHERS_VIEW_ID;
+const airtableTableEventsId = process.env.AIRTABLE_TABLE_EVENTS_ID;
+const airtableTableEventsViewId = process.env.AIRTABLE_TABLE_EVENTS_VIEW_ID;
 
 async function getFilmEvents() {
-    try {
-        const res = await fetch(`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableFilmEventId}?view=${airtableTableFilmEventViewId}`, {
-            headers: {
-                Authorization: `Bearer ${airtableApiKey}`,
-            },
-            cache: 'no-store'
-        });
-        const data = await res.json();
-        return data.records
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const res = await fetch(
+			`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableFilmEventId}?view=${airtableTableFilmEventViewId}`,
+			{
+				headers: {
+					Authorization: `Bearer ${airtablePersonalAccessToken}`,
+				},
+				cache: 'no-store',
+			}
+		);
+		const data = await res.json();
+		return data.records;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 async function getFilms() {
-    try {
-        const res = await fetch(`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableId}?view=${airtableTableFilmsViewId}`, {
-            headers: {
-                Authorization: `Bearer ${airtableApiKey}`,
-            },
-            cache: 'no-store'
-        });
-        const data = await res.json();
-        //console.log('data records', data.records[0]);
-        return data
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const res = await fetch(
+			`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableId}?view=${airtableTableFilmsViewId}`,
+			{
+				headers: {
+					Authorization: `Bearer ${airtablePersonalAccessToken}`,
+				},
+				cache: 'no-store',
+			}
+		);
+		const data = await res.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
 async function getEvents() {
-    try {
-      const res = await fetch(`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableEventsId}?view=${airtableTableEventsViewId}`, {
-        headers: {
-          Authorization: `Bearer ${airtableApiKey}`,
-        },
-        cache: 'no-store'
-      });
-      const data = await res.json();
-      //console.log('events', data.records)
-      return data.records
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-async function getOthers() {
-    try {
-        //`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableOthersId}?view=${airtableTableOthersViewId}`
-        const res = await fetch(`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableOthersId}`, {
-            headers: {
-                Authorization: `Bearer ${airtableApiKey}`,
-            },
-            cache: 'no-store'
-        });
-        const data = await res.json();
-        // console.log('all others', data)
-        return data.records
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		const res = await fetch(
+			`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableEventsId}?view=${airtableTableEventsViewId}`,
+			{
+				headers: {
+					Authorization: `Bearer ${airtablePersonalAccessToken}`,
+				},
+				cache: 'no-store',
+			}
+		);
+		const data = await res.json();
+		return data.records;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
 }
 
+async function getOthers() {
+	try {
+		//`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableOthersId}?view=${airtableTableOthersViewId}`
+		const res = await fetch(
+			`https://api.airtable.com/v0/${airtableBaseId}/${airtableTableOthersId}`,
+			{
+				headers: {
+					Authorization: `Bearer ${airtablePersonalAccessToken}`,
+				},
+				cache: 'no-store',
+			}
+		);
+		const data = await res.json();
+		return data.records;
+	} catch (error) {
+		console.log(error);
+		return [];
+	}
+}
 
-const Dynamicp5TestTwo = dynamic(
-    () => import('./Testp5Two/Testp5Two'),
-    {
-        ssr: false,
-        loading: () => <P5preloader />
-        
-    }
-)
+const Dynamicp5TestTwo = dynamic(() => import('./Testp5Two/Testp5Two'), {
+	ssr: false,
+	loading: () => <P5preloader />,
+});
 
 export default async function HomeView({ language }) {
-    const otherEvents = await getEvents()
-    let allEvents = [...otherEvents.filter(evt => evt.fields.ShowInCalendar)]
-    let films = await getFilms()
-    const filmEvents = await getFilmEvents()
-    for (let film of films.records) {
-        const filmId = film.id
-        const eventsOfFilm = filmEvents.filter(event => event.fields.Film[0] === filmId)
-        film.fields['Events'] = eventsOfFilm
-        allEvents = [...allEvents, ...eventsOfFilm, film]
-    }
-    console.log('all event count', allEvents.length)
-    const others = await getOthers()
-    const marquee = others.filter(data => data.fields['Type'] === 'Donate-Float').map(marquee => marquee.fields[`Title_${language}`]).join('');
-    const sponsors = others.filter(data => data.fields['Type'] === 'Sponsor').map(sponsor => {
-        sponsor.fields['Img'] = sponsor.fields['Img'] ? dropboxUrl(sponsor.fields['Img']) : 'hi'
-        return sponsor
-    })
-    const partners = others.filter(data => data.fields['Type'] === 'Partner').map(sponsor => {
-        sponsor.fields['Img'] = sponsor.fields['Img'] ? dropboxUrl(sponsor.fields['Img']) : 'hi'
-        return sponsor
-    })
-    const questions = others.filter(data => data.fields['Type'] === 'Question')
-    const websiteGlobal = others.filter(data => data.fields['Type'] === 'Website')[0]
-    const aboutThisYear = others.filter(data => data.fields['Type'] === 'About-This-Year')
-    const websiteGlobalFields = websiteGlobal.fields
-    const heroText = websiteGlobalFields[`Title_${language}`].split('\n')
-    const { VenueLink, TrailerLink, GoogleCalendarUrl } = websiteGlobalFields
-    const sectionText = sectionTitles[language]
-    const { filmSectionTitle, aboutSectionTitle, eventSectionTitle, sponsorSectionTitle, partnerSectionTitle, questionSectionTitle } = sectionText
-    return (
-        <div id="content" className='relative'>
-            <div className='w-full min-h-screen flex flex-col justify-center isolate relative z-[60]'>
-                <LanguageSelect />
-                <div className="py-10 mix-blend text-shadow">
-                    {/* <Dynamicp5TestTwo /> */}
-                    <h1 className={`text-center text-h1 font-special text-primary ${language === 'tw' ? 'font-semibold' : ''}`}>{websiteGlobalFields[`Theme_${language}`]}</h1>
-                    {heroText.map((text, i)=> <h1 className={`text-center text-h1 font-special ${language === 'tw' && (i === 0 ) ? 'font-semibold' : ''}`}>{text}</h1>)}
-                </div>
-                <div className='text-center z-50'>
-                    <Modal language={language} trailerUrl={TrailerLink} venueLink={VenueLink} />
-                </div>
-            </div>
+	const otherEvents = await getEvents();
 
-            <Marquee content={marquee} link={`/${language}/donate`} />
+	let allEvents =
+		otherEvents !== undefined
+			? [...otherEvents.filter((evt) => evt.fields.ShowInCalendar)]
+			: [];
+	let films = await getFilms();
+	const filmEvents = await getFilmEvents();
+	for (let film of films.records) {
+		const filmId = film.id;
 
-            <section className="max-w-1440 mx-auto px-[5vw]">
-                <SpecialTitle year={websiteGlobalFields['Year']} title={aboutSectionTitle} img="../img/hero2Img.png" />
-                <div>
-                    {aboutThisYear?.map(obj => <div key={obj.id} className="my-4">
-                        <h2 className="text-center font-special text-h2 font-semibold mb-2">{obj.fields[`Question_${language}`]}</h2>
-                        <div>
-                            <RichText content={obj.fields[`Answer_${language}`]}/>
-                        </div>
-                    </div>)}
-                </div>
-                <SectionTitle content={filmSectionTitle}></SectionTitle>
+		const eventsOfFilm = filmEvents.filter((event) => {
+			return event.fields.Film?.[0] === filmId;
+		});
+		film.fields['Events'] = eventsOfFilm;
+		allEvents = [...allEvents, ...eventsOfFilm, film];
+	}
+	console.log('all event count', allEvents.length);
+	const others = await getOthers();
+	const marquee = others
+		.filter((data) => data.fields['Type'] === 'Donate-Float')
+		.map((marquee) => marquee.fields[`Title_${language}`])
+		.join('');
+	const sponsors = others
+		.filter((data) => data.fields['Type'] === 'Sponsor')
+		.map((sponsor) => {
+			sponsor.fields['Img'] = sponsor.fields['Img']
+				? dropboxUrl(sponsor.fields['Img'])
+				: 'hi';
+			return sponsor;
+		});
+	const partners = others
+		.filter((data) => data.fields['Type'] === 'Partner')
+		.map((sponsor) => {
+			sponsor.fields['Img'] = sponsor.fields['Img']
+				? dropboxUrl(sponsor.fields['Img'])
+				: 'hi';
+			return sponsor;
+		});
+	const questions = others.filter((data) => data.fields['Type'] === 'Question');
+	const websiteGlobal = others.filter(
+		(data) => data.fields['Type'] === 'Website'
+	)[0];
+	const aboutThisYear = others.filter(
+		(data) => data.fields['Type'] === 'About-This-Year'
+	);
+	const websiteGlobalFields = websiteGlobal.fields;
+	const heroText = websiteGlobalFields[`Title_${language}`].split('\n');
+	const { VenueLink, TrailerLink, GoogleCalendarUrl } = websiteGlobalFields;
+	const sectionText = sectionTitles[language];
+	const {
+		filmSectionTitle,
+		aboutSectionTitle,
+		eventSectionTitle,
+		sponsorSectionTitle,
+		partnerSectionTitle,
+		questionSectionTitle,
+	} = sectionText;
+	return (
+		<div id='content' className='relative'>
+			<div className='w-full min-h-screen flex flex-col justify-center isolate relative z-[60]'>
+				<LanguageSelect />
+				<div className='py-10 mix-blend text-shadow'>
+					{/* <Dynamicp5TestTwo /> */}
+					<h1
+						className={`text-center text-h1 font-special text-primary ${
+							language === 'tw' ? 'font-semibold' : ''
+						}`}
+					>
+						{websiteGlobalFields[`Theme_${language}`]}
+					</h1>
+					{heroText.map((text, i) => (
+						<h1
+							className={`text-center text-h1 font-special ${
+								language === 'tw' && i === 0 ? 'font-semibold' : ''
+							}`}
+						>
+							{text}
+						</h1>
+					))}
+				</div>
+				<div className='text-center z-50'>
+					<Modal
+						language={language}
+						trailerUrl={TrailerLink}
+						venueLink={VenueLink}
+					/>
+				</div>
+			</div>
 
-                <div className=''>
-                    {films.records.map(film =>
-                        !isEmpty(film.fields) && <Film
-                            key={film.id}
-                            id={film.id}
-                            language={language}
-                            film={film.fields}
-                        >
-                        </Film>
-                    )}
-                </div>
+			<Marquee content={marquee} link={`/${language}/donate`} />
 
-                <SectionTitle content={eventSectionTitle}></SectionTitle>
-                <Events events={otherEvents}language={language} />
+			<section className='max-w-1440 mx-auto px-[5vw]'>
+				<SpecialTitle
+					year={websiteGlobalFields['Year']}
+					title={aboutSectionTitle}
+					img='../img/hero2Img.png'
+				/>
+				<div>
+					{aboutThisYear?.map((obj) => (
+						<div key={obj.id} className='my-4'>
+							<h2 className='text-center font-special text-h2 font-semibold mb-2'>
+								{obj.fields[`Question_${language}`]}
+							</h2>
+							<div>
+								<RichText content={obj.fields[`Answer_${language}`]} />
+							</div>
+						</div>
+					))}
+				</div>
+				<SectionTitle content={filmSectionTitle}></SectionTitle>
 
-                <SectionTitle content={sponsorSectionTitle}></SectionTitle>
-                <Sponsors language={language} sponsors={sponsors} />
+				<div className=''>
+					{films.records.map(
+						(film) =>
+							!isEmpty(film.fields) && (
+								<Film
+									key={film.id}
+									id={film.id}
+									language={language}
+									film={film.fields}
+								></Film>
+							)
+					)}
+				</div>
 
-                <SectionTitle content={partnerSectionTitle}></SectionTitle>
-                <Sponsors language={language} sponsors={partners} />
+				<SectionTitle content={eventSectionTitle}></SectionTitle>
+				<Events events={otherEvents} language={language} />
 
-                <SectionTitle content={questionSectionTitle}></SectionTitle>
-                <Questions language={language} questions={questions} />
-                
-                <Calendar events={allEvents} language={language}/>
-                <Footer language={language} googleCalendar={GoogleCalendarUrl}/>
-            </section>
-        </div>
-    )
+				<SectionTitle content={sponsorSectionTitle}></SectionTitle>
+				<Sponsors language={language} sponsors={sponsors} />
+
+				<SectionTitle content={partnerSectionTitle}></SectionTitle>
+				<Sponsors language={language} sponsors={partners} />
+
+				<SectionTitle content={questionSectionTitle}></SectionTitle>
+				<Questions language={language} questions={questions} />
+
+				<Calendar events={allEvents} language={language} />
+				<Footer language={language} googleCalendar={GoogleCalendarUrl} />
+			</section>
+		</div>
+	);
 }
--- utils/dropboxUrl.js
+++ utils/dropboxUrl.js
@@ -1,3 +1,6 @@
 export default function dropboxUrl(url) {
-    return url.replace(/&dl=0(?!.*&dl=0)/, "&raw=1").replace("www.dropbox.com", "dl.dropbox.com")
+	if (!url) return '';
+	return url
+		.replace(/&dl=0(?!.*&dl=0)/, '&raw=1')
+		.replace('www.dropbox.com', 'dl.dropbox.com');
 }
