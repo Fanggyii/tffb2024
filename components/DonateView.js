956ab45e8bd9ed4fc8f63e7826013ad824c788e4
lowpolyfox <alan.maldonado.lara1@gmail.com>
2024-10-07 00:07:27 +0100

    Add ?. to fix array indexing error

--- components/DonateView.js
+++ components/DonateView.js
@@ -41,7 +41,7 @@ export default async function DonateView({ language }) {
 			<LanguageSelect link={['/donate', '/de/donate', '/tw/donate']} />
 			<section className='max-w-1440 mx-auto px-[5vw]'>
 				<BackHome link={`/${language}`} language={language} />
-				<CopyBank bank={donate[0]} language={language} />
+				<CopyBank bank={donate?.[0]} language={language} />
 				<Footer language={language} googleCalendar={GoogleCalendarUrl} />
 			</section>
 		</>
