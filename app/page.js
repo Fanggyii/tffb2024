7e9dc4609ed6e849695a3f64a057c3ec18f0df26
lowpolyfox <alan.maldonado.lara1@gmail.com>
2024-10-08 10:55:52 +0100

    Make page revalidate/dynamic

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
-import './globals.css'
-import './style.scss'
-import HomeView from '../components/HomeView'
+import './globals.css';
+import './style.scss';
+import HomeView from '../components/HomeView';
 
 export default async function Page() {
-  return (
-    <>
-      <HomeView language={'en'} />
-    </>
-  )
+	return (
+		<>
+			<HomeView language={'en'} />
+		</>
+	);
 }
+
+export const revalidate = 10;
+export const dynamic = 'auto';
